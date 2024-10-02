1cf89bcdc4c2c1642aa5a27c312db80a5b7dd90c
abrahamzahed <43421279+abrahamzahed@users.noreply.github.com>
2024-10-02 15:29:50 -0700

    Update 2_rag_chain.py

--- 2_rag_chain.py
+++ 2_rag_chain.py
@@ -12,11 +12,17 @@ from langchain_core.runnables import RunnablePassthrough
 ## Enable MLflow Tracing
 mlflow.langchain.autolog()
 
-#Get the configs from the local conf file
+#Import configs from rag_model_config.yaml file
 model_config = mlflow.models.ModelConfig(development_config='rag_model_config.yaml')
 databricks_resources = model_config.get("databricks_resources")
 retriever_config = model_config.get("retriever_config")
 llm_config = model_config.get("llm_config")
+
+
+#The following functions will extract the user query string and previous messages
+#The user query string will be passed to the chain as the question
+#The user query string + the chat history will be passed as the context for the vector search
+#The chat history will be passed to the LLM with the vector search results
 
 # Return the string contents of the most recent message from the user
 def extract_user_query_string(chat_messages_array):
@@ -80,13 +86,18 @@ model = ChatDatabricks(
 )
 
 # RAG Chain
+# 1. question
+# 2. question + chat history -> vector search -> search results
+# 3. question + search results + chat history -> prompt
+# 4. prompt -> model
+# 5. model -> answer
 chain = (
     {
         "question": itemgetter("messages") | RunnableLambda(extract_user_query_string),
-        "context": itemgetter("messages")
-        | RunnableLambda(combine_all_messages_for_vector_search)
-        | vector_search_as_retriever
-        | RunnableLambda(format_context),
+        "context": itemgetter("messages") 
+            | RunnableLambda(combine_all_messages_for_vector_search)
+            | vector_search_as_retriever
+            | RunnableLambda(format_context),
         "chat_history": itemgetter("messages") | RunnableLambda(extract_previous_messages)
     }
     | prompt
