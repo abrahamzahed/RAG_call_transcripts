d95c48dfaee13ea825aa320ab63d1686c6d038d1
abrahamzahed <43421279+abrahamzahed@users.noreply.github.com>
2024-10-02 15:03:31 -0700

    Update log_register_model.py

--- log_register_model.py
+++ log_register_model.py
@@ -1,7 +1,7 @@
 # Log the model to MLflow
 with mlflow.start_run(run_name=databricks_resources.get("mlflow_run_name")):
     logged_chain_info = mlflow.langchain.log_model(
-        lc_model=os.path.join(os.getcwd(), 'rag_chain.py'),  # Chain code file e.g., /path/to/the/rag_chain.py 
+        lc_model=os.path.join(os.getcwd(), '2_rag_chain.py'),  # Chain code file e.g., /path/to/the/2_rag_chain.py 
         model_config='rag_model_config.yaml',  # Chain configuration 
         artifact_path=databricks_resources.get("artifact_path"),  # Required by MLflow
         input_example=model_config.get("input_example"),  # Save the chain's input schema.  MLflow will execute the chain before logging & capture it's output schema.
